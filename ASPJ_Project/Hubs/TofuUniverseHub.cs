f0a5a8f656ce8d26a93cc684831ba184a47e9353
Unknown <angjinkuan@hotmail.sg>
2017-10-30 07:44:30 +0800

    Added SignalR dependency

---
+++ ASPJ_Project/Hubs/TofuUniverseHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace TofuUniverse
+{
+    public class TofuUniverseHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}

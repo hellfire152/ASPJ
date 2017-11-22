9b94e132bdae4350bf7779a6d04c9372a4210f8f
RexxarAng <rexar.aa@gmail.com>
2017-11-23 00:47:52 +0800

    dynamic creation of threads

--- ASPJ_Project/Controllers/ForumController.cs
+++ ASPJ_Project/Controllers/ForumController.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASPJ_Project.Models;
+using ASPJ_Project.ViewModels;
 
 namespace ASPJ_Project.Controllers
 {
     public class ForumController : Controller
     {
         // GET: Forum
-        public ActionResult Index()
+        public ActionResult Index(string currentFilter, string searchString, int? page)
         {
+            if(searchString != null)
+            {
+                page = 1;
+            } 
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentView = currentFilter;
             return View();
         }
         public ActionResult Home()
         {
-            return View();
+            var threads = new List<Thread>
+            {
+                new Thread{ title = "Flash is missing", username = "Alex Ang", PostedDate = DateTime.Now, votes = 0 },
+                new Thread{ title = "Flash is missing", username = "Alex Ang", PostedDate = DateTime.Now, votes = 0 },
+                new Thread{ title = "Flash is missing", username = "Alex Ang", PostedDate = DateTime.Now, votes = 0 }
+            };
+
+            var viewModel = new ForumViewModel
+            {
+                Threads = threads
+            };
+            return View(viewModel);
         }
         public ActionResult Thread()
         {
---
+++ ASPJ_Project/ViewModels/ForumViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ASPJ_Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPJ_Project.ViewModels
+{
+    public class ForumViewModel
+    {
+        public List<Thread> Threads;
+    }
+}

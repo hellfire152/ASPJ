daa1815cdd652233779951811843a69a62180dc6
NvrHere <solomoncasual@gmail.com>
2017-11-29 19:46:33 +0800

    DB Connection

--- ASPJ_Project/Global.asax.cs
+++ ASPJ_Project/Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using MySql.Data;
+using MySql.Data.MySqlClient;
 
 namespace ASPJ_Project
 {
@@ -18,4 +20,32 @@ namespace ASPJ_Project
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
+
+    public class DubuBaseConnect 
+    {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        MySql.Data.MySqlClient.MySqlCommand cmd;
+        String queryStr;
+
+        private void DubuInit()
+        {
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["WebAppConnString"].ToString();
+
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+            conn.Open();
+            Console.Write("Connection Success");
+            queryStr = " ";
+
+            /*queryStr = "INSERT INTO dububase.accounts (userName, password, email, phoneNumber)" + 
+                "VALUES"(firstname.Text + lastname.Text + "," + phoneNumber.Text)";*/
+
+            cmd = new MySql.Data.MySqlClient.MySqlCommand(queryStr, conn);
+
+            cmd.ExecuteReader();
+
+            conn.Close();
+        }
+    }
 }
+
+

86613cf5d3e6f336141c453154ae01a8b10b43f2
NvrHere <solomoncasual@gmail.com>
2017-12-03 20:19:11 +0800

    a little stuff
    Its a little but i cannot find my file so ya just commit everything

--- ASPJ_Project/Models/Shop.cs
+++ ASPJ_Project/Models/Shop.cs
@@ -67,12 +67,12 @@ namespace ASPJ_Project.Models
                 case "tps":
                     if(u.Effect.Item2.Length == 1)
                     {
-                        switch(u.Effect.Item2[0])
+                        /*switch(u.Effect.Item2[0])
                         {
                             case '+':
                                 this.BaseTps + u.Effect.Item3;
                                 break;
-                        }
+                        }*/
                     }
                     break;
                 case "cost":

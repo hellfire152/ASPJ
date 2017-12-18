585f2b82cbaa796321f89cc5777e2df806ae5238
JohnTheFool <johnfoohw@gmail.com>
2017-12-18 18:13:30 +0800

    Stuff

---
+++ ASPJ_Project/Controllers/ShopController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPJ_Project.Models;
+using ASPJ_Project.ViewModels;
+
+namespace ASPJ_Project.Controllers
+{
+    public class ShopController : Controller
+    {
+        // GET: Shop
+        public ActionResult Shop()
+        {
+            var shopItems = new List<PremiumShop.PremiumItem>
+            {
+                new PremiumShop.PremiumItem{ itemName= "Fedora Hat", itemType= "Hat", itemDescription= "Mi'lady.", beansPrice= 100},
+                new PremiumShop.PremiumItem{ itemName= "Karate Headband", itemType= "Hat", itemDescription= "Hiya!", beansPrice= 75}
+            };
+            return View();
+        }
+        public ActionResult PremiumShop()
+        {
+            return View();
+        }
+    }
+}

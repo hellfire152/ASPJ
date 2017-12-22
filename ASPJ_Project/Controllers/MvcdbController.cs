b50f5908e84dcb2f79c0a2f1f21c6fd11e758a35
NvrHere <solomoncasual@gmail.com>
2017-12-22 14:53:47 +0800

    Stable Ver 1.3
    - Database retrival success
    - Only tested retrieval at the moment with a template

---
+++ ASPJ_Project/Controllers/MvcdbController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPJ_Project.Models;
+
+namespace ASPJ_Project.Controllers
+{
+    public class MvcdbController : Controller
+    {
+        // GET: Mvcdb
+        public ActionResult Index()
+        {
+            List<user> userList = new List<user>();
+            using(mvccruddbEntities dbModel = new mvccruddbEntities())
+            {
+                userList = dbModel.users.ToList<user>();
+            }
+            return View(userList);
+        }
+
+        // GET: Mvcdb/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Mvcdb/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Mvcdb/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mvcdb/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Mvcdb/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Mvcdb/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Mvcdb/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}

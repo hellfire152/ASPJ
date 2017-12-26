265ec035a2579d1c1ef38201e607ab413c513e01
JohnTheFool <johnfoohw@gmail.com>
2017-12-26 22:23:22 +0800

    Beans Purchasing

--- ASPJ_Project/Controllers/ShopController.cs
+++ ASPJ_Project/Controllers/ShopController.cs
@@ -18,9 +18,16 @@ namespace ASPJ_Project.Controllers
                 new PremiumShop.PremiumItem{ itemName= "Fedora Hat", itemType= "Hat", itemDescription= "Mi'lady.", beansPrice= 100},
                 new PremiumShop.PremiumItem{ itemName= "Karate Headband", itemType= "Hat", itemDescription= "Hiya!", beansPrice= 75}
             };
+
+            var currentUser = new PremiumShop.User { username = "jhn905", beansAmount = 400 };
+
             return View();
         }
-        public ActionResult PremiumShop()
+        public ActionResult BeansPurchase()
+        {
+            return View();
+        }
+        public ActionResult PurchaseConfirmation()
         {
             return View();
         }

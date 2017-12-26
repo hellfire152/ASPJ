5e7041bb45cc92b9c8e088a628b4d8168a6d4e2b
NvrHere <solomoncasual@gmail.com>
2017-12-26 15:56:26 +0800

    Stable 1.4.3
    - Template for Signalr (Chat Socket)

--- ASPJ_Project/Controllers/HomeController.cs
+++ ASPJ_Project/Controllers/HomeController.cs
@@ -26,5 +26,10 @@ namespace ASPJ_Project.Controllers
 
             return View();
         }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
     }
 }
--- ASPJ_Project/Hubs/Startup.cs
+++ ASPJ_Project/Hubs/Startup.cs
@@ -12,6 +12,7 @@ namespace TofuUniverse
     {
         public void Configuration(IAppBuilder app)
         {
+            // Any connection or hub wire up and configuration should go here
             app.MapSignalR();
         }
     }

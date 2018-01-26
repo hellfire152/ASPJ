319d08d61d57a7b38c8ab548dd96ebaf97499fb2
NvrHere <solomoncasual@gmail.com>
2018-01-26 10:45:08 +0800

    Stable 1.5.10
    Removed the model should be stable

--- ASPJ_Project/Controllers/MvcdbController.cs
+++ ASPJ_Project/Controllers/MvcdbController.cs
@@ -19,163 +19,163 @@ namespace ASPJ_Project.Controllers
     public class MvcdbController : Controller
     {
 
-        private mvccruddbEntities dbModel = new mvccruddbEntities();
+        //private mvccruddbEntities dbModel = new mvccruddbEntities();
         // GET: Mvcdb
-        public ActionResult Index()
-        {
-            List<user> userList = new List<user>();
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                IList<string> censoredWords = new List<string>
-                {
-                  "gosh",
-                  "drat",
-                  "darn*",
-                  "fuck*",
-                  "idiot",
-                  "stupid",
-                  "ji bai",
-                  "bitch",
-                  "pussy",
-                  "shit",
-                  "cb",
+        //public ActionResult Index()
+        //{
+        //    List<user> userList = new List<user>();
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        IList<string> censoredWords = new List<string>
+        //        {
+        //          "gosh",
+        //          "drat",
+        //          "darn*",
+        //          "fuck*",
+        //          "idiot",
+        //          "stupid",
+        //          "ji bai",
+        //          "bitch",
+        //          "pussy",
+        //          "shit",
+        //          "cb",
 
-                };
+        //        };
 
-                //Censor censor = new Censor(censoredWords);
-                //string result;
+        //        //Censor censor = new Censor(censoredWords);
+        //        //string result;
 
-                //result = censor.CensorText("I stubbed my toe. Gosh it hurts!");
-                //// I stubbed my toe. **** it hurts!
+        //        //result = censor.CensorText("I stubbed my toe. Gosh it hurts!");
+        //        //// I stubbed my toe. **** it hurts!
 
-                //result = censor.CensorText("The midrate on the USD -> EUR forex trade has soured my day. Drat!");
-                //// The midrate on the USD -> EUR forex trade has soured my day. ****!
+        //        //result = censor.CensorText("The midrate on the USD -> EUR forex trade has soured my day. Drat!");
+        //        //// The midrate on the USD -> EUR forex trade has soured my day. ****!
 
-                //result = censor.CensorText("Gosh darnit, my shoe laces are undone.");
-                userList = dbModel.users.ToList<user>();
-                //string censoredUserList;
-                //foreach (var item in userList)
-                //{
-                //    censoredUserList = censor.CensorText(item.FirstName);
-                //}
+        //        //result = censor.CensorText("Gosh darnit, my shoe laces are undone.");
+        //        userList = dbModel.users.ToList<user>();
+        //        //string censoredUserList;
+        //        //foreach (var item in userList)
+        //        //{
+        //        //    censoredUserList = censor.CensorText(item.FirstName);
+        //        //}
 
-            }
-            return View(userList);
-        }
+        //    }
+        //    return View(userList);
+        //}
 
-        public static void Encrypt(string value)
-        {
-            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
-            {
-                UTF8Encoding utf8 = new UTF8Encoding();
-                byte[] data = md5.ComputeHash(utf8.GetBytes(value));
-                Convert.ToBase64String(data);
-            }
+        //public static void Encrypt(string value)
+        //{
+        //    using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+        //    {
+        //        UTF8Encoding utf8 = new UTF8Encoding();
+        //        byte[] data = md5.ComputeHash(utf8.GetBytes(value));
+        //        Convert.ToBase64String(data);
+        //    }
 
-        }
+        //}
 
         // GET: Mvcdb/Details/5
-        public ActionResult Details(int id)
-        {
-            user userModel = new user();
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
-            }
-            return View(userModel);
-        }
+        //public ActionResult Details(int id)
+        //{
+        //    user userModel = new user();
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
+        //    }
+        //    return View(userModel);
+        //}
 
         // GET: Mvcdb/Create
-        public ActionResult Create()
-        {
-            return View(new user());
-        }
+        //public ActionResult Create()
+        //{
+        //    return View(new user());
+        ////}
 
         // POST: Mvcdb/Create
-        [HttpPost]
-        public ActionResult Create(user userModel)
-        {            
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                //AESCryptoStuff aes_obj = new AESCryptoStuff();
-                //userModel = aes_obj.AesEncrypt(userModel);
-                dbModel.users.Add(userModel);
-                dbModel.SaveChanges();
-            }
-            return RedirectToAction("Index");
-        }
+        //[HttpPost]
+        //public ActionResult Create(user userModel)
+        //{            
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        //AESCryptoStuff aes_obj = new AESCryptoStuff();
+        //        //userModel = aes_obj.AesEncrypt(userModel);
+        //        dbModel.users.Add(userModel);
+        //        dbModel.SaveChanges();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
         // GET: Mvcdb/Edit/5
-        public ActionResult Edit(int id)
-        {
-            user userModel = new user();
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
-            }
-            return View(userModel);
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    user userModel = new user();
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
+        //    }
+        //    return View(userModel);
+        //}
 
         // POST: Mvcdb/Edit/5
-        [HttpPost]
-        public ActionResult Edit(user userModel)
-        {
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                dbModel.Entry(userModel).State = System.Data.Entity.EntityState.Modified;
-                dbModel.SaveChanges();
-            }
-            return RedirectToAction("Index");
-        }
+        //[HttpPost]
+        //public ActionResult Edit(user userModel)
+        //{
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        dbModel.Entry(userModel).State = System.Data.Entity.EntityState.Modified;
+        //        dbModel.SaveChanges();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
         // GET: Mvcdb/Delete/5
-        public ActionResult Delete(int id)
-        {
-            user userModel = new user();
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
-            }
-            return View(userModel);
-        }
+        //public ActionResult Delete(int id)
+        //{
+        //    user userModel = new user();
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
+        //    }
+        //    return View(userModel);
+        //}
 
         // POST: Mvcdb/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                user userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
-                dbModel.users.Remove(userModel);
-                dbModel.SaveChanges();
-            }
-            return RedirectToAction("Index");
-        }
+        //[HttpPost]
+        //public ActionResult Delete(int id, FormCollection collection)
+        //{
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        user userModel = dbModel.users.Where(x => x.UserID == id).FirstOrDefault();
+        //        dbModel.users.Remove(userModel);
+        //        dbModel.SaveChanges();
+        //    }
+        //    return RedirectToAction("Index");
+        //}
 
-        public ActionResult ChatTest()
-        {
-            List<user> userList = new List<user>();
-            using (mvccruddbEntities dbModel = new mvccruddbEntities())
-            {
-                userList = dbModel.users.ToList<user>();
-            }
+        //public ActionResult ChatTest()
+        //{
+        //    List<user> userList = new List<user>();
+        //    using (mvccruddbEntities dbModel = new mvccruddbEntities())
+        //    {
+        //        userList = dbModel.users.ToList<user>();
+        //    }
 
-            return View(userList);
-        }
+        //    return View(userList);
+        //}
 
-        public ActionResult FriendList(string searchString)
-        {
-            List<user> userList = new List<user>();
-            var searchList = from c in dbModel.users select c;
-            if (!string.IsNullOrEmpty(searchString))
-            {
-                //searchList = searchList.Where(c => c.FirstName == searchString);
-                searchList = searchList.Where(c => c.FirstName.Contains(searchString));
-            }
-            //userList = dbModel.users.ToList<user>();
+        //public ActionResult FriendList(string searchString)
+        //{
+        //    List<user> userList = new List<user>();
+        //    var searchList = from c in dbModel.users select c;
+        //    if (!string.IsNullOrEmpty(searchString))
+        //    {
+        //        //searchList = searchList.Where(c => c.FirstName == searchString);
+        //        searchList = searchList.Where(c => c.FirstName.Contains(searchString));
+        //    }
+        //    //userList = dbModel.users.ToList<user>();
 
-            return View(searchList);
-        }
+        //    return View(searchList);
+        //}
 
         public ActionResult Chat()
         {

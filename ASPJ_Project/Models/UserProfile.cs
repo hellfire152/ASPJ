2eedff2ea0e4f25b83498e7f61d69904ae9f3edc
Unknown <angjinkuan@hotmail.sg>
2018-02-02 23:42:43 +0800

    Added Eunice's stuff

---
+++ ASPJ_Project/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPJ_Project.Models
+{
+    public class UserProfile
+    {
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Username { get; set; }
+        
+    }
+}

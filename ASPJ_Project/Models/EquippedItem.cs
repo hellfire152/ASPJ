5af37e076a6d2253a0ccaf626fcef2f9f4765ff7
JohnTheFool <johnfoohw@gmail.com>
2018-02-03 16:31:48 +0800

    Equip Items

---
+++ ASPJ_Project/Models/EquippedItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPJ_Project.Models
+{
+    public class EquippedItem
+    {
+        public int userID { get; set; }
+        public int equippedHat { get; set; }
+        public int equippedOutfit { get; set; }
+    }
+}

319ea296da4a713659147141b44f8b9acf244268
Unknown <angjinkuan@hotmail.sg>
2018-02-03 12:12:09 +0800

    Added eunice's dependencies

---
+++ ASPJ_Project/Models/Hmac.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace ASPJ_Project.Models
+{
+    public class Hmac
+    {
+        public static Hmac CurrentInstance;
+
+        public static void Initialize(string key)
+        {
+            byte[] keyArr = Encoding.ASCII.GetBytes(key);
+            CurrentInstance = new Hmac(keyArr);
+        }
+
+        private HMACSHA1 h;
+
+        public Hmac(byte[] key)
+        {
+            h = new HMACSHA1(key);
+        }
+
+        //return a url encoded string of the input
+        public string Encode(string input)
+        {
+            byte[] messageBytes = Encoding.ASCII.GetBytes(input);
+            byte[] encodedBytes = h.ComputeHash(messageBytes);
+            string encodedMessage = Encoding.ASCII.GetString(encodedBytes);
+            return HttpContext.Current.Server.UrlEncode(encodedMessage);
+        }
+    }
+}
